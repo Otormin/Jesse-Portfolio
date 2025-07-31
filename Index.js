87085376270965fa160c97aa73c21709437bff7f
Otormin <yjesse330@gmail.com>
2025-07-31 01:28:12 +0100

    Fixed contact page

--- Index.js
+++ Index.js
@@ -24,9 +24,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // Contact form handling
 document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
     alert('Thank you for your message! I will get back to you soon.');
-    this.reset();
 });
 
 // Navbar scroll effect
